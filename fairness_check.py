3d7cb098d2b09f2c2ea46b9c666ad4e139ea64e2
Kunal Gawand <143700746+Kunalgawand03@users.noreply.github.com>
2025-12-30 11:25:36 +0530

    Delete fairness_check.py

--- fairness_check.py
+++
@@ -1,8 +0,0 @@
-def fairness_check(text):
-    bias_words = ["boys", "girls", "weak", "stupid"]
-    found = [w for w in bias_words if w in text.lower()]
-
-    if not found:
-        return "No bias detected"
-    else:
-        return f"Potential bias words found: {found}"
